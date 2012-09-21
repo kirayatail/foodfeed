6a50e6f9f8ad214ff349c0d7597dba3eb8686209
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-09-21 12:07:48 +0200

    Fixed some errors in configuration that prevented hibernate from working.

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/EntityDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/EntityDao.java
@@ -5,12 +5,12 @@ import java.util.*;
 
 public interface EntityDao<T, ID extends Serializable> {
 
-	T save(T entity);
+	public void save(T entity);
 
-	void delete(T entity);
+	public void delete(T entity);
 
-	T findById(ID id);
+	public T findById(ID id);
 
-	List<T> findAll();
+	public List<T> findAll();
 
 }
--- src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
@@ -3,26 +3,24 @@ package edu.chl.dat076.foodfeed.model.dao;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
-import org.springframework.orm.hibernate3.HibernateTemplate;
 
 import edu.chl.dat076.foodfeed.model.entity.Recipe;
 
 public class RecipeDao implements EntityDao<Recipe, Long> {
 
-	private HibernateTemplate hibernateTemplate;
+	private SessionFactory sessionFactory;
 
 	public void setSessionFactory(SessionFactory sessionFactory) {
 
-		this.hibernateTemplate = new HibernateTemplate(sessionFactory);
+		this.sessionFactory = sessionFactory;
 	}
 
-	public Recipe save(Recipe recipe) {
-		// TODO Auto-generated method stub
-		return null;
+	public void save(Recipe recipe) {
+		sessionFactory.openSession().saveOrUpdate(recipe);
 	}
 
 	public void delete(Recipe recipe) {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub
 	}
 
 	public Recipe findById(Long id) {
@@ -31,8 +29,7 @@ public class RecipeDao implements EntityDao<Recipe, Long> {
 	}
 
 	public List<Recipe> findAll() {
-		// TODO Auto-generated method stub
-	    return null;
+		return sessionFactory.openSession().createQuery("from Recipe").list();
 	}
-
+	
 }
