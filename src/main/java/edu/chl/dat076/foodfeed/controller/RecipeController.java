ef40df5f6119bd28e190ab5f54e4b8ab2a8c4ad7
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-09-23 22:54:11 +0200

    Configured Spring Security to enable logging in.

--- src/main/java/edu/chl/dat076/foodfeed/controller/RecipeController.java
+++ src/main/java/edu/chl/dat076/foodfeed/controller/RecipeController.java
@@ -1,5 +1,7 @@
 package edu.chl.dat076.foodfeed.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 import edu.chl.dat076.foodfeed.model.dao.RecipeDao;
 import edu.chl.dat076.foodfeed.model.entity.Recipe;
