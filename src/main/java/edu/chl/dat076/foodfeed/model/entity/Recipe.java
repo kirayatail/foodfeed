ec0735561c1ea8bfcbcedcaa1b4830ea6a4929ac
kirayatail <kirayatail@gmail.com>
2012-09-30 20:54:25 +0200

    Created a separate class for ingredients. Work is not done.

--- src/main/java/edu/chl/dat076/foodfeed/model/entity/Recipe.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/entity/Recipe.java
@@ -10,6 +10,10 @@ public class Recipe {
 	private Long id;
 
 	private String name;
+	
+	private String description;
+
+	
 
 	@Id
 	@GeneratedValue
@@ -27,6 +31,14 @@ public class Recipe {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
