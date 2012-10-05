0562e9213bed5c328548d77fe6bb66cd413a6c88
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-10-05 16:19:15 +0200

    Injection of recipeDao with annotations instead of xml.

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
@@ -4,17 +4,16 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import edu.chl.dat076.foodfeed.model.entity.Recipe;
 
+@Repository("recipeDao")
 public class RecipeDao implements EntityDao<Recipe, Long> {
 
+	@Autowired
 	private SessionFactory sessionFactory;
-
-	public void setSessionFactory(SessionFactory sessionFactory) {
-
-		this.sessionFactory = sessionFactory;
-	}
 
 	public void save(Recipe recipe) {
 		sessionFactory.openSession().saveOrUpdate(recipe);
