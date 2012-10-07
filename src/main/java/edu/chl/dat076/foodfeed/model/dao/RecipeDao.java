b5d3441e21bb96344fd1b15febad0c4d42722f7b
Marika Hansson <hmarika@student.chalmers.se>
2012-10-07 11:55:34 +0200

    Added some code for delete

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import edu.chl.dat076.foodfeed.model.entity.Recipe;
+import org.hibernate.Query;
 
 @Repository("recipeDao")
 public class RecipeDao implements EntityDao<Recipe, Long> {
@@ -15,19 +16,25 @@ public class RecipeDao implements EntityDao<Recipe, Long> {
 	@Autowired
 	private SessionFactory sessionFactory;
 
+        @Override
 	public void save(Recipe recipe) {
 		sessionFactory.getCurrentSession().saveOrUpdate(recipe);
 	}
 
+        @Override
 	public void delete(Recipe recipe) {
-		// TODO Auto-generated method stub
+		Query q = sessionFactory.getCurrentSession().createQuery("delete from Recipe r where id=:rid");
+                q.setParameter("rid", recipe.getId());
+                q.executeUpdate();
 	}
 
+        @Override
 	public Recipe findById(Long id) {
 		return (Recipe) sessionFactory.getCurrentSession().get(Recipe.class, id);
 	}
 
 	@SuppressWarnings("unchecked")
+        @Override
 	public List<Recipe> findAll() {
 		return (List<Recipe>) sessionFactory.getCurrentSession().createQuery("from Recipe").list();
 	}
