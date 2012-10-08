7732eb9130cf15b70ace7f1d806b21d54c4f9c48
Marcus Stigelid <marcus.stigelid@gmail.com>
2012-10-08 15:13:26 +0200

    Added validation constraint for the password field in User entity

--- src/main/java/edu/chl/dat076/foodfeed/model/entity/User.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/entity/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 
@@ -18,6 +19,7 @@ public class User implements Serializable {
     
     @NotNull
     @Size(min = 6)
+    @Pattern(regexp = "[a-zA-Z0-9_\\s\\W]")
     private String password;
     
     @OneToMany
