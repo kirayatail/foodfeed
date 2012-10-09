b3117261e0d86e5527e2a789a1cf0523e7a9c350
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-10-09 09:47:38 +0200

    Made sure user had a setter for ID to avoid hibernate errors. Also added setId() to IEntity.

--- src/main/java/edu/chl/dat076/foodfeed/model/entity/Ingredient.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/entity/Ingredient.java
@@ -7,64 +7,58 @@ import javax.validation.constraints.NotNull;
 
 /**
  * Specifies a separate ingredient to be used in relation to recipes.
- * 
- * TODO: Connect to some kind of quantity in order to ease conversions between units.
- * @author max
  *
+ * TODO: Connect to some kind of quantity in order to ease conversions between
+ * units.
  */
 @Entity
-public class Ingredient implements IEntity<Long>, Serializable{
+public class Ingredient implements IEntity<Long>, Serializable {
 
+    private Long id;
+    private Grocery grocery;
+    private double amount;
+    private String unit;
 
-	private Long id;
-	
-        private Grocery grocery;
-	
-	private double amount;        
- 
-	private String unit;
-	
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        @Override
-	public Long getId() {
-            return this.id;
-	}
-        
-	public void setId(Long id) {
-            this.id = id;
-	}
-        
-        @OneToOne
-	public Grocery getGrocery() {
-            return grocery;
-	}
-        
-	public void setGrocery(Grocery grocery) {
-            this.grocery = grocery;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Override
+    public Long getId() {
+        return this.id;
+    }
 
-        @Min(value = 0, message = "Value must be positive")
-	public double getAmount() {
-            return amount;
-	}
-        
-	public void setAmount(double amount) {
-            this.amount = amount;
-	}
-        
-        @NotNull
-	public String getUnit() {
-            return unit;
-	}
-        
-	public void setUnit(String unit) {
-            this.unit = unit;
-	}
-        
-        @Override
-        public String toString(){
-            return grocery.toString()+" "+String.format("%.2f", this.amount)+" "+unit;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    @OneToOne
+    public Grocery getGrocery() {
+        return grocery;
+    }
+
+    public void setGrocery(Grocery grocery) {
+        this.grocery = grocery;
+    }
+
+    @Min(value = 0, message = "Value must be positive")
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @NotNull
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return grocery.toString() + " " + String.format("%.2f", this.amount) + " " + unit;
+    }
 }
