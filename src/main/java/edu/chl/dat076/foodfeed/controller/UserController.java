5ee23280e1ad53b1902680d2ac340003753a7b53
Max Witt <kirayatail@gmail.com>
2012-10-11 17:16:08 +0200

    introduced method signatures to register, show and getLoginForm in UserController

--- src/main/java/edu/chl/dat076/foodfeed/controller/UserController.java
+++ src/main/java/edu/chl/dat076/foodfeed/controller/UserController.java
@@ -5,12 +5,35 @@
 package edu.chl.dat076.foodfeed.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Serves Registration and view of user profiles.
  * 
  */
 @Controller
+@RequestMapping(value="/user/")
 public class UserController {
+    
+    @RequestMapping(value="/", method = RequestMethod.POST)
+    public String doRegister(){
+        return null;
+    }
+    
+    @RequestMapping(value="/register", method = RequestMethod.GET)
+    public String getRegisterForm(){
+        return null;
+    }
+    
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable long id, Model model){
+        
+        return null;
+    }
+    
+    
     
 }
