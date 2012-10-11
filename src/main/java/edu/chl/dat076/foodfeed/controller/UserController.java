25784a95f172e0df7da53df35a36ab13aa77a667
Max Witt <kirayatail@gmail.com>
2012-10-11 16:28:20 +0200

    Created UserController

---
+++ src/main/java/edu/chl/dat076/foodfeed/controller/UserController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.dat076.foodfeed.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Serves Registration and view of user profiles.
+ * 
+ */
+@Controller
+public class UserController {
+    
+}
