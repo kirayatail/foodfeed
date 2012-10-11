57468ccd040c12c524bf2c7356262eae6e899f3d
Marika Hansson <hmarika@student.chalmers.se>
2012-10-11 16:43:18 +0200

    Added getByIngredient Added direction @Annotation to recipes for the ingredient list

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
@@ -1,12 +1,24 @@
 package edu.chl.dat076.foodfeed.model.dao;
 
+import edu.chl.dat076.foodfeed.model.entity.Ingredient;
 import edu.chl.dat076.foodfeed.model.entity.Recipe;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository("recipeDao")
 public class RecipeDao extends AbstractDao<Recipe, Long> {
 
+    protected EntityManager entityManager;
+    
     RecipeDao() {
         super(Recipe.class);
+    }
+    
+    public List<Recipe> getByIngredient(Ingredient i){
+        Query res= entityManager.createQuery("select r from Recipe r where r.id=(select ri.recipeid from Recipe_Ingredient where ri.ingredientid =:i_id)");
+        res.setParameter("i_id", i.getId());
+        return res.getResultList();
     }
 }
