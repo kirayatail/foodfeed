582b80d6005b41a0c1cd508d4b0fad670148e48d
Max Witt <kirayatail@gmail.com>
2012-10-12 14:50:02 +0200

    UserDao done for the time being. Possible changes may include 'getUserByRecipe' but it's not prioritized.

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/UserDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/UserDao.java
@@ -2,11 +2,9 @@
 package edu.chl.dat076.foodfeed.model.dao;
 
 import edu.chl.dat076.foodfeed.model.entity.User;
+import org.springframework.stereotype.Repository;
 
-/**
- *
- * 
- */
+@Repository("userDao")
 public class UserDao extends AbstractDao<User, Long> {
     
     public UserDao(){
