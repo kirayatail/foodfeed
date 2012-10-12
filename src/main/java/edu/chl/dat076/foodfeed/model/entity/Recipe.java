65dac5d427f1c3dceac6d371029892e950d80050
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-10-12 15:15:10 +0200

    Moved constraints and annotations to instance variables in Recipe.

--- src/main/java/edu/chl/dat076/foodfeed/model/entity/Recipe.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/entity/Recipe.java
@@ -11,55 +11,51 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Recipe implements IEntity<Long>, Serializable{
+public class Recipe implements IEntity<Long>, Serializable {
 
-        private long id;
-        
-        private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    @Size(min = 1, message = "The name must consist of at least one character")
+    private String name;
+    @NotNull
+    @Size(min = 1, message = "The description must consist of at least one character")
+    private String description;
+    @OneToMany
+    private List<Ingredient> ingredients;
 
-        private String description;
+    @Override
+    public Long getId() {
+        return id;
+    }
 
-        @OneToMany
-        private List<Ingredient> ingredients;
-        
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        @Override
-        public Long getId() {
-            return id;
-        }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    public String getName() {
+        return name;
+    }
 
-        @NotNull
-        @Size(min = 1, message = "The name must consist of at least one character")
-        public String getName() {
-            return name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-        
-        @NotNull
-        @Size(min = 1, message = "The description must consist of at least one character")
-        public String getDescription() {
-            return description;
-        }
+    public String getDescription() {
+        return description;
+    }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-        @OneToMany
-        public List<Ingredient> getIngredients() {
-            return ingredients;
-        }
+    @OneToMany
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
 
-        public void setIngredients(List<Ingredient> ingredients) {
-            this.ingredients = ingredients;
-        }
-       
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 }
