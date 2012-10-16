bd485b77e4ac66161a38eec5dd96b064125bd4ce
Max Witt <kirayatail@gmail.com>
2012-10-16 17:21:11 +0200

    Changed ID type from Long to String, which matches the entity username type

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/UserDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/UserDao.java
@@ -5,7 +5,7 @@ import edu.chl.dat076.foodfeed.model.entity.User;
 import org.springframework.stereotype.Repository;
 
 @Repository("userDao")
-public class UserDao extends AbstractDao<User, Long> {
+public class UserDao extends AbstractDao<User, String> {
     
     public UserDao(){
         super(User.class);
