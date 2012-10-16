6e2ec495db5943a71e720c390d529a18d5cbed23
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-10-16 20:42:02 +0200

    Some additions to the config for tests.

--- src/test/java/edu/chl/dat076/foodfeed/model/dao/RecipeDaoTest.java
+++ src/test/java/edu/chl/dat076/foodfeed/model/dao/RecipeDaoTest.java
@@ -15,7 +15,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration(locations = {
+    "classpath*:spring/root-context.xml"})
 @Transactional
 public class RecipeDaoTest {
 
