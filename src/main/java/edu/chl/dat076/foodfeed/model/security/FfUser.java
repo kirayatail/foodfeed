afc3ea2dbfe98d80d46c17efcfa3f1b09f736b89
Max Witt <kirayatail@gmail.com>
2012-10-16 17:06:38 +0200

    Added FfUser, an implementation of UserDetails which maps our entity User to the UserDetails requirements

---
+++ src/main/java/edu/chl/dat076/foodfeed/model/security/FfUser.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.dat076.foodfeed.model.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Implements SpringSecurity's UserDetails and roughly corresponds to SpringS's User.
+ * As our users don't support locking and activation of accounts, all these properties are set to true
+ * Any new implementation of the user model requires a modification of this class
+ * to incorporate the new model information instead of static data
+ * (includes authority roles, expiration or locking)
+ * 
+ */
+public class FfUser implements UserDetails {
+
+    private String password;
+    
+    private String username;
+    
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> auths = new ArrayList<>();
+        auths.add(new GrantedAuthority(){
+
+            @Override
+            public String getAuthority() {
+                return "ROLE_USER";
+            }
+            
+        });
+        
+        return auths;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
