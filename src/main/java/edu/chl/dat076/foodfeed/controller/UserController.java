fd98073ff0a067a981944b4012f9fda1469b0cf0
Max Witt <kirayatail@gmail.com>
2012-10-17 16:40:44 +0200

    Modified UserController and UserDAO to make sure that users are inserted into the DB and passwords are encoded correctly. By this commit, the authentication module uses our user entities instead of static users. Fixes #7

--- src/main/java/edu/chl/dat076/foodfeed/controller/UserController.java
+++ src/main/java/edu/chl/dat076/foodfeed/controller/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  */
 @Controller
+@Transactional
 @RequestMapping("/user")
 public class UserController {
     
