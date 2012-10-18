3c109398d98b39b5ebec58b891187747e167554d
Max Witt <kirayatail@gmail.com>
2012-10-18 17:47:06 +0200

    Hibernate does not do an insert anymore when calling UserDao.create(), see diff from previous commit. Added more logging to concerned classes.

--- src/main/java/edu/chl/dat076/foodfeed/controller/UserController.java
+++ src/main/java/edu/chl/dat076/foodfeed/controller/UserController.java
@@ -3,12 +3,20 @@ package edu.chl.dat076.foodfeed.controller;
 
 import edu.chl.dat076.foodfeed.model.dao.UserDao;
 import edu.chl.dat076.foodfeed.model.entity.User;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,16 +33,24 @@ public class UserController {
     
     @Autowired
     private UserDao userDao;
+    
+    @Autowired
+    @Qualifier("org.springframework.security.authenticationManager")
+    protected AuthenticationManager authenticationManager;
 
     private static final Logger logger = LoggerFactory
             .getLogger(UserController.class);
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String doRegister(Model model, @Validated User usr) {
+    public String doRegister(Model model, @Validated User usr, BindingResult result, HttpServletRequest request) {
         logger.info("Registering new user");
+        logger.info("Provided password: "+usr.getPassword());
         userDao.create(usr);
-        // TODO: Some more black magic to be done here (make sure the user is 
-        // automatically logged in
+        logger.info("Password after DB create: "+usr.getPassword());
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(usr.getUsername(), usr.getPassword());
+        Authentication auth = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        
         return "redirect:/";
     }
 
@@ -48,12 +64,28 @@ public class UserController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String show(@PathVariable String id, Model model) {
+    public String showById(@PathVariable String id, Model model) {
         logger.info("showing user with id: "+id);
         model.addAttribute("user", userDao.find(id));
         return "user/show";
     }
     
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String show(Model model){
+        logger.info("showing the authenticated user");
+        
+        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        
+        
+        logger.info("the shown principal is "+auth.getName());
+        
+        User usr = userDao.find(auth.getName());
+        
+        model.addAttribute("user", usr);
+        
+        return "user/show";
+    }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginForm() {
