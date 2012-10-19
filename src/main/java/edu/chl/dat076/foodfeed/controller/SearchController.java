b15b89b461277a1e414b606c8876d4edf89936e4
Marcus Stigelid <marcus.stigelid@gmail.com>
2012-10-19 14:04:30 +0200

    Added search form and views and controller for it

---
+++ src/main/java/edu/chl/dat076/foodfeed/controller/SearchController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.dat076.foodfeed.controller;
+
+import edu.chl.dat076.foodfeed.model.dao.RecipeDao;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class SearchController {
+
+    @Autowired
+    private RecipeDao recipeDao;
+    private static final Logger logger = LoggerFactory
+            .getLogger(SearchController.class);
+
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public String search(Model model, @RequestParam(value="query") String query) {
+        
+        //recipeDao.getByName(query);
+        
+        model.addAttribute("query", query);
+        
+        return "search/result";
+
+    }
+}
