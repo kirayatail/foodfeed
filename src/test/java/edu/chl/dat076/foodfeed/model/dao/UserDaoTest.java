d9f536a440628b94472b1a80f6dbd058d29f8701
Marika Hansson <hmarika@student.chalmers.se>
2012-10-19 16:19:16 +0200

    added missing annotations to test class -> works

--- src/test/java/edu/chl/dat076/foodfeed/model/dao/UserDaoTest.java
+++ src/test/java/edu/chl/dat076/foodfeed/model/dao/UserDaoTest.java
@@ -12,12 +12,21 @@ import junit.framework.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Kohina
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+    "classpath*:spring/root-context.xml",
+    "classpath*:spring/security-context.xml"})
+@Transactional
 public class UserDaoTest {
     
     @Autowired
