2d82cc4bfef3ed8924d3ed6b6bf95a69ebec933d
Marika <hmarika@student.chalmers.se>
2012-10-19 17:18:42 +0200

    removed unused imports

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/UserDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/UserDao.java
@@ -5,7 +5,6 @@ import edu.chl.dat076.foodfeed.model.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository("userDao")
 public class UserDao extends AbstractDao<User, String> {
