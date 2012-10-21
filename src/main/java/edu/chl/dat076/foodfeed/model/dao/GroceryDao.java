4831b920aaa7f9f3073fd41a4577c37daec2dd7d
Marcus Stigelid <marcus.stigelid@gmail.com>
2012-10-21 13:33:15 +0200

    Removed unnecessary comments

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/GroceryDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/GroceryDao.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.chl.dat076.foodfeed.model.dao;
 
 import edu.chl.dat076.foodfeed.model.entity.Grocery;
