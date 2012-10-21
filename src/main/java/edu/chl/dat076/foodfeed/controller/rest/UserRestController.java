7f5803f946b6bde3b74ffa3c7dc6627f5f6b096a
Marika <hmarika@student.chalmers.se>
2012-10-21 13:15:29 +0200

    cleaned unused imports

--- src/main/java/edu/chl/dat076/foodfeed/controller/rest/UserRestController.java
+++ src/main/java/edu/chl/dat076/foodfeed/controller/rest/UserRestController.java
@@ -12,8 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
