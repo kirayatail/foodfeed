5b87eb55b7084669237d8a5713ba5d8d8a7e49ab
Max Witt <kirayatail@gmail.com>
2012-10-21 10:43:20 +0200

    User REST: Read works but there are still issues with create. Have not looked into login yet.

--- src/main/java/edu/chl/dat076/foodfeed/controller/rest/UserRestController.java
+++ src/main/java/edu/chl/dat076/foodfeed/controller/rest/UserRestController.java
@@ -4,8 +4,16 @@
  */
 package edu.chl.dat076.foodfeed.controller.rest;
 
+import edu.chl.dat076.foodfeed.model.dao.UserDao;
 import edu.chl.dat076.foodfeed.model.entity.User;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -13,15 +21,46 @@ import org.springframework.web.bind.annotation.*;
  * 
  */
 @Controller
-@RequestMapping(value="rs/user")
+@RequestMapping("rs/user")
 public class UserRestController {
+    
+    @Autowired
+    private UserDao userDao;
+    
+    private static final Logger logger = LoggerFactory
+            .getLogger(UserRestController.class);
+    
     
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody // IMPORTANT! declares that the returned object is the ACTUAL thing to return
-                  // and not a reference to a jspx page
-    public User getSample(){
-        User usr = new User("imax","badanka");
+                  // and not a reference to a jspx page. JSON will be created through black magic.
+    public List<User> getAll(){
         
-        return usr;
-    }    
+        /*
+         * Here's the point where all users would have their passwords removed 
+         * before sending them to the response
+         */
+        
+        return userDao.findAll();
+    }
+    
+    @RequestMapping(value = "/name/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public User getByID(@PathVariable String id){
+        
+        /*
+         * Place for a check to see if the password should be sent or not
+         * (maybe never send passwords, regardless of logged in or not?)
+         */
+        return userDao.find(id);
+    }
+    
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@RequestParam("username") String username, @RequestParam("password") String pass){
+        User usr = new User(username, pass);
+        userDao.create(usr);
+        
+    }
 }
