a114c555ae32d3130578be63c0b001ea41923537
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-10-22 10:21:15 +0200

    Fixed getByGrocery (previously called getByIngredient).

--- src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/dao/RecipeDao.java
@@ -1,5 +1,6 @@
 package edu.chl.dat076.foodfeed.model.dao;
 
+import edu.chl.dat076.foodfeed.model.entity.Grocery;
 import edu.chl.dat076.foodfeed.model.entity.Ingredient;
 import edu.chl.dat076.foodfeed.model.entity.Recipe;
 import java.util.List;
@@ -14,9 +15,9 @@ public class RecipeDao extends AbstractDao<Recipe, Long> {
         super(Recipe.class);
     }
     
-    public List<Recipe> getByIngredient(Ingredient i){
-        TypedQuery<Recipe> res= entityManager.createQuery("select r from Recipe r where r.id=(select ri.recipeid from Recipe_Ingredient where ri.ingredientid =:i_id)", Recipe.class);
-        res.setParameter("i_id", i.getId());
+    public List<Recipe> getByGrocery(Grocery g){
+        TypedQuery<Recipe> res= entityManager.createQuery("select r from Recipe r left join r.ingredients as i where i.grocery = :grocery)", Recipe.class);
+        res.setParameter("grocery", g);
         return res.getResultList();
     }
     
