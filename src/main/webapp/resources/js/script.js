ffcbff871a5f492b19f67c05d83f56092d007315
Marika <hmarika@student.chalmers.se>
2012-10-23 15:56:13 +0200

    Removed if -> can now add recipe without ingredients

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -3,30 +3,28 @@ function handleRem(button){
         
     var at = button.value;
     
-    if($('.ingredient').length > 1){
-        $('.ingredient-index-'+at).remove();
+    $('.ingredient-index-'+at).remove();
 
-        for(var i=at; i<counter; i++){
-            $('.ingredient-index-'+(parseInt(i)+1)).attr("class", 'ingredient clearfix ingredient-index-'+i);
+    for(var i=at; i<counter; i++){
+        $('.ingredient-index-'+(parseInt(i)+1)).attr("class", 'ingredient clearfix ingredient-index-'+i);
             
-            $('.grocery-label-'+(parseInt(i)+1)).attr("for", 'ingredients'+i+'.grocery.id');
-            $('.grocery-label-'+(parseInt(i)+1)).attr("class", 'grocery-label-'+i);
-            $('#ingredients'+(parseInt(i)+1)+'\\.grocery\\.id').attr("name", 'ingredients['+i+'].grocery.id');
-            $('#ingredients'+(parseInt(i)+1)+'\\.grocery\\.id').attr("id", 'ingredients'+i+'.grocery.id');
+        $('.grocery-label-'+(parseInt(i)+1)).attr("for", 'ingredients'+i+'.grocery.id');
+        $('.grocery-label-'+(parseInt(i)+1)).attr("class", 'grocery-label-'+i);
+        $('#ingredients'+(parseInt(i)+1)+'\\.grocery\\.id').attr("name", 'ingredients['+i+'].grocery.id');
+        $('#ingredients'+(parseInt(i)+1)+'\\.grocery\\.id').attr("id", 'ingredients'+i+'.grocery.id');
 
-            $('.amount-label-'+(parseInt(i)+1)).attr("for", 'ingredients'+i+'.amount');
-            $('.amount-label-'+(parseInt(i)+1)).attr("class", 'amount-label-'+i);
-            $('#ingredients'+(parseInt(i)+1)+'\\.amount').attr("name", 'ingredients['+i+'].amount');
-            $('#ingredients'+(parseInt(i)+1)+'\\.amount').attr("id", 'ingredients'+i+'.amount');
+        $('.amount-label-'+(parseInt(i)+1)).attr("for", 'ingredients'+i+'.amount');
+        $('.amount-label-'+(parseInt(i)+1)).attr("class", 'amount-label-'+i);
+        $('#ingredients'+(parseInt(i)+1)+'\\.amount').attr("name", 'ingredients['+i+'].amount');
+        $('#ingredients'+(parseInt(i)+1)+'\\.amount').attr("id", 'ingredients'+i+'.amount');
 
-            $('.unit-label-'+(parseInt(i)+1)).attr("for", 'ingredients'+i+'.unit');
-            $('.unit-label-'+(parseInt(i)+1)).attr("class", 'unit-label-'+i);
-            $('#ingredients'+(parseInt(i)+1)+'\\.unit').attr("name", 'ingredients['+i+'].unit');
-            $('#ingredients'+(parseInt(i)+1)+'\\.unit').attr("id", 'ingredients'+i+'.unit');
+        $('.unit-label-'+(parseInt(i)+1)).attr("for", 'ingredients'+i+'.unit');
+        $('.unit-label-'+(parseInt(i)+1)).attr("class", 'unit-label-'+i);
+        $('#ingredients'+(parseInt(i)+1)+'\\.unit').attr("name", 'ingredients['+i+'].unit');
+        $('#ingredients'+(parseInt(i)+1)+'\\.unit').attr("id", 'ingredients'+i+'.unit');
             
-            $('.remove-index-'+(parseInt(i)+1)).attr("value", i);
-            $('.remove-index-'+(parseInt(i)+1)).attr("class", 'button remove-ingredient remove-index-'+i);
-        }
+        $('.remove-index-'+(parseInt(i)+1)).attr("value", i);
+        $('.remove-index-'+(parseInt(i)+1)).attr("class", 'button remove-ingredient remove-index-'+i);
     }
 }
 
