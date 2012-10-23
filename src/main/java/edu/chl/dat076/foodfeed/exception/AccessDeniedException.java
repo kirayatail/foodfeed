cb697406c97db64e4e9ca18283461d9e61748a99
Marika Hansson <hmarika@student.chalmers.se>
2012-10-23 16:40:11 +0200

    moved class comment

--- src/main/java/edu/chl/dat076/foodfeed/exception/AccessDeniedException.java
+++ src/main/java/edu/chl/dat076/foodfeed/exception/AccessDeniedException.java
@@ -1,11 +1,11 @@
-/**
- * Exception that results in a response 404.
- */
 package edu.chl.dat076.foodfeed.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception that results in a response 404.
+ */
 @ResponseStatus(value = HttpStatus.FORBIDDEN)
 public class AccessDeniedException extends RuntimeException {
 }
