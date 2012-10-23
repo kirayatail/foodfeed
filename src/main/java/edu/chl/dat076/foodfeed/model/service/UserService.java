1615e09409d6b4962decd872f5c77be4110530a9
Marika <hmarika@student.chalmers.se>
2012-10-23 16:50:04 +0200

    Added class comment

--- src/main/java/edu/chl/dat076/foodfeed/model/service/UserService.java
+++ src/main/java/edu/chl/dat076/foodfeed/model/service/UserService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/*
+ * A help class for communications between userDao and userController
+ */
 @Service("userService")
 @Transactional
 public class UserService {
