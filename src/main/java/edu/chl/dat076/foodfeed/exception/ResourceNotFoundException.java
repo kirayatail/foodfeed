0747a516ef10f89576db45d93b1e28701cbfd202
Marika <hmarika@student.chalmers.se>
2012-10-23 16:40:11 +0200

    moved class comment

--- src/main/java/edu/chl/dat076/foodfeed/exception/ResourceNotFoundException.java
+++ src/main/java/edu/chl/dat076/foodfeed/exception/ResourceNotFoundException.java
@@ -1,11 +1,11 @@
-/**
- * Exception that results in a response 404.
- */
 package edu.chl.dat076.foodfeed.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception that results in a response 404.
+ */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 }
